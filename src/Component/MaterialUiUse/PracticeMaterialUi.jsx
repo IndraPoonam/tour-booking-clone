067505f2595c3d8256bc2630c7e83b84123d4fc8
IndraPoonam <iamindra.poonam@gmail.com>
2024-05-20 13:26:16 +0530

    hello

--- src/Component/MaterialUiUse/PracticeMaterialUi.jsx
+++ src/Component/MaterialUiUse/PracticeMaterialUi.jsx
@@ -1,11 +1,22 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, {useState} from 'react'
 const PracticeMaterialUi = () => {
+  const[color,setColor]= useState("primary")
+  function coustom(){
+    setColor("secondary")
+    // alert("FUNCTION CALLED BY POONAM")
+  }
   return (
     <>
-    <div style={{textAlign: "center", background: 'yellow', text:"black", padding:'3rem'}}>
-    <h1>React-MUI</h1>
-    <Button color='primary' variant="contained">Click Me</Button></div>
+      <div style={{ textAlign: "center", background: 'yellow', text: "black", padding: '3rem' }}>
+        <h1>React-MUI</h1>
+        <Button
+          // color='primary'
+          color={color}
+          variant="contained"
+          // disabled                        //its means button show but it is not working
+          onClick={()=>{coustom()}}
+        >Click Me</Button></div>
     </>
   )
 }
